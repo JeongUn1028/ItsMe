8606f5973ad14e23a62b62fe993576afad2282e4
JeongUn1028 <wjddns363@naver.com>
2026-03-26 17:50:06 +0900

    test(project): enviroment setting and sample test

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
 
   return (
     <main>
+      <h1>Next.js 기반 개인 포트폴리오 프로젝트</h1>
       <section>
         {projects.map((project) => (
           <PortfolioCard key={project.slug} {...project} />
