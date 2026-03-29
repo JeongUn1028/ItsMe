8cc8b60898d0a02ee4664fe6952e62a565542739
JeongUn1028 <wjddns363@naver.com>
2026-03-29 20:41:23 +0900

    feat(UI): set tailwind css

--- app/components/home/contact.tsx
+++ app/components/home/contact.tsx
@@ -4,29 +4,27 @@ import style from "./contact.module.css";
 export default function Contact() {
   return (
     <div
-      className={`${style.container} glass col-span-1 row-span-1 h-full xl:col-start-2 xl:row-start-2`}
+      className={`glass col-span-1 row-span-1 xl:col-start-2 xl:row-start-2 ${style.card}`}
     >
-      <h2 className={style.title}>Contact</h2>
-      <div className={style.subContainer}>
-        <div className={style.contacts}>
+      <h2 className={style.title}>CONTACT</h2>
+      <div className={style.content}>
+        <div className={style.row}>
           <Image
             src="/email_icon.png"
             alt="Email Icon"
             width={18}
             height={18}
           />
-          <div>:</div>
-          <div className={style.email}>wjddns363@naver.com</div>
+          :<div className={style.value}>wjddns363@naver.com</div>
         </div>
-        <div className={style.contacts}>
+        <div className={style.row}>
           <Image
             src="/phone_icon.png"
             alt="Phone Icon"
             width={18}
             height={18}
           />
-          <div>:</div>
-          <div className={style.email}>010-9656-1295</div>
+          :<div className={style.value}>010-9656-1295</div>
         </div>
       </div>
     </div>
--- app/components/home/header.tsx
+++ app/components/home/header.tsx
@@ -1,9 +1,9 @@
-import styles from "./header.module.css";
+import style from "./header.module.css";
 
 export default function Header() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.logo}>LEEJEONGUN.COM</h1>
+    <div className={style.container}>
+      <h1 className={style.title}>LEEJEONGUN.COM</h1>
       <div>modeChanger</div>
     </div>
   );
