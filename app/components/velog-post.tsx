fcfcd32efb6f5f0e44266cc6e4b91724885ab53f
JeongUn1028 <wjddns363@naver.com>
2026-03-30 16:41:37 +0900

    refactor(project): velog list component refactoring

---
+++ app/components/velog-post.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import style from "./velog-post.module.css";
+
+export default function VelogPost({
+  post,
+}: {
+  post: {
+    title: string;
+    tags: string[];
+    url_slug: string;
+    released_at: string;
+  };
+}) {
+  return (
+    <Link
+      href={`https://velog.io/@jeongun1028/${post.url_slug}`}
+      key={post.title}
+      target="_blank"
+    >
+      <div className={style.container}>
+        <h2 className={style.title}>{post.title}</h2>
+        <div className={style.tag}>
+          {post.tags.map((tag) => (
+            <p key={tag}>{tag}</p>
+          ))}
+        </div>
+        <div className={style.dateContainer}>
+          <p>작성 일자 : {new Date(post.released_at).toLocaleDateString()}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
